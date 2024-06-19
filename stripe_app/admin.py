51c1f5207e03675a0acd61b9eeba2762ba744e3b
DisonUyoga <disonobudho233@gmail.com>
2024-06-19 09:34:05 +0300

    host

--- stripe_app/admin.py
+++ stripe_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Payment
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=("id","name","price", "quantity", "created_at")
+
+@admin.register(Payment)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=("user", "amount")

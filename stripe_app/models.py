4f19a9f74fee5bbe5187cbdb80ab7e83e02d5015
DisonUyoga <disonobudho233@gmail.com>
2024-06-19 09:39:00 +0300

    host

--- stripe_app/models.py
+++ stripe_app/models.py
@@ -11,5 +11,5 @@ class Product(models.Model):
     description=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
 class Payment(models.Model):
-    user=models.CharField(max_length=255, unique=True)
+    user=models.CharField(max_length=255)
     amount=models.DecimalField(max_digits=10, decimal_places=2)
